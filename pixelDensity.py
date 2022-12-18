32f15c7e520513000c47ed63ed2dd4f3f683a28f
matteggers <94013259+matteggers@users.noreply.github.com>
2022-12-17 21:27:42 -0500

    testing

---
+++ pixelDensity.py
@@ -0,0 +1,23 @@
+import math
+
+
+commonDimensions = {"27":
+    
+    }
+
+math.sqrt((w*w)+(h*h))
+
+
+
+def pixelDensity():
+    
+
+
+
+asas
+
+
+
+
+
+#size = input("What is the diagonal screen size in inches?")
