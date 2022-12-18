50d96f02cfd5e917d89e3fbf27a4ddba7deff850
matteggers <94013259+matteggers@users.noreply.github.com>
2022-12-17 22:16:02 -0500

    Properly labeled all values and ensured results are rounded to two decimal places.

--- pixelDensity.py
+++ pixelDensity.py
@@ -1,49 +1,23 @@
 import math
 #coding based on formulae found in: https://www.calculatorsoup.com/calculators/technology/ppi-calculator.php
 
-
-#commonDimensions = {'24': [20.918, 11.766],
-#                    '24.5': [21.354, 12.011]
-#    
-#    }
-#
-#
-#
-##math.sqrt((w*w)+(h*h))
-#
-#def pixelDensity(size):
-#    values = list(commonDimensions.get(size))
-#    w = values[0]
-#    h = values[1]
-#    
-#    pixDens = math.sqrt((w*w)+(h*h))
-#    print(pixDens)
-#    return pixDens
-#    
-#
-#size = input("What is the diagonal screen size in inches? ")
-#
-#
-#pixelDensity(size)
-
-
-def diagonalPixels(size):
+def pixelDensity(diagonalSize):
     w = 1920
     h = 1080
-    res = math.sqrt((w*w) + (h*h))
-    rounded_res = round(res, 2)
     
-    PPI = rounded_res // size
+    diagonalPixels = math.sqrt((w*w) + (h*h))
+    rounded_diagonalPixels = round(diagonalPixels, 2)
+    
+    PPI = round((rounded_diagonalPixels / diagonalSize), 2)
     print(PPI)
     
-    #print(res)
-    return res, PPI
-
-size = input("Diagonal size of monitor: ")
+    return diagonalSize, rounded_diagonalPixels, PPI
 
 
+diagonalSize = input("Diagonal size of monitor: ")
 
-diagonalPixels(int(size))
+
+pixelDensity(float(diagonalSize))
 
 
 
