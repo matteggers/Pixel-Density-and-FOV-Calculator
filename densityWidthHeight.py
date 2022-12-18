f545ee357f670b17fe49ae0159192b701822063a
matteggers <94013259+matteggers@users.noreply.github.com>
2022-12-18 00:37:00 -0500

    Not tested, this is my skyrim autosave

--- densityWidthHeight.py
+++ densityWidthHeight.py
@@ -1,16 +1,64 @@
+import math
+#from pixelDensity import pixelDensity, resolution
     #w = commonDimensions.get(diagonalSize[0])
     #h = commonDimensions.get(diagonalSize[1])
     
-    #commonDimensions = { #in inches, couldn't I have done this in excel?
-#    "24": [20.918, 11.766],
-#    "24.5": [21.354, 12.011],
-#    "27": [23.533, 13.237],
-#    "32": [27.89, 15.688],
-#    "34": [29.634, 16.669],
-#    "50": [43.579, 24.513],
-#    "55": [47.937, 26.964],
-#    "60": [52.295, 29.416],
-#    "65": [56.652, 31.867],
-#    "70": [61.01, 34.318],
-#    "74.9": [65.281, 36.721],
-#}
+commonDimensions = { #in inches, couldn't I have done this in excel?
+    "24": [20.918, 11.766],
+    "24.5": [21.354, 12.011],
+    "27": [23.533, 13.237],
+    "32": [27.89, 15.688],
+    "34": [29.634, 16.669],
+    "50": [43.579, 24.513],
+    "55": [47.937, 26.964],
+    "60": [52.295, 29.416],
+    "65": [56.652, 31.867],
+    "70": [61.01, 34.318],
+    "74.9": [65.281, 36.721],
+}
+
+#can find the horizontal width of any screen by dividing height / (16/9). inversly can do so by width x (16/9)
+
+
+def pythagoras(): #Diagonal screen size in inches
+    global rounded_hypotenuse
+    
+    hypotenuse = math.sqrt(((w*w) + (h*h)))
+    rounded_hypotenuse = round(hypotenuse)
+    return rounded_hypotenuse
+
+
+def sizeCheck(rounded_hypotenuse):
+    global pixelWidth
+    global pixelHeight
+    
+    
+    if 27 > rounded_hypotenuse > 21:
+        pixelWidth = 1920
+        pixelHeight = 1080
+    elif 34 >= rounded_hypotenuse >= 27:
+        pixelWidth = 2560
+        pixelHeight = 1440
+    elif 75 >= rounded_hypotenuse >= 50:
+        pixelWidth = 3840
+        pixelHeight = 2160
+    elif 75 > rounded_hypotenuse:
+        pixelWidth = 7680
+        pixelHeight = 4320
+    else:
+        print("Value not within range.")
+    
+def pixelDensity(rounded_hypotenuse):
+    diagonalPixels = math.sqrt(((pixelWidth*pixelWidth) + (pixelHeight*pixelHeight)))
+    rounded_diagonalPixels = round(diagonalPixels, 2)
+    
+    
+    PPI = round((rounded_diagonalPixels / rounded_hypotenuse), 2)
+    print(f"{PPI} pixels per inch")
+    
+    return rounded_hypotenuse, rounded_diagonalPixels, PPI
+
+    
+    
+w = float(input("How wide would you like the screen in inches? "))
+h = float(input("How tall would you like the screen in inches? "))
