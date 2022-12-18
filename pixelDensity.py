9caecabfa7216c8e589f4f11514b9659ccca64a8
matteggers <94013259+matteggers@users.noreply.github.com>
2022-12-17 21:45:16 -0500

    progress yet no where at the same time

--- pixelDensity.py
+++ pixelDensity.py
@@ -1,23 +1,25 @@
 import math
+#coding based on formulae found in: https://www.calculatorsoup.com/calculators/technology/ppi-calculator.php
 
 
-commonDimensions = {"27":
+commonDimensions = {'24': [20.918, 11.766],
+                    '24.5': [21.354, 12.011]
     
     }
 
-math.sqrt((w*w)+(h*h))
+#math.sqrt((w*w)+(h*h))
 
-
-
-def pixelDensity():
+def pixelDensity(size):
+    values = list(commonDimensions.get(size))
+    w = values[0]
+    h = values[1]
+    
+    pixDens = math.sqrt((w*w)+(h*h))
+    print(pixDens)
+    return pixDens
     
 
+size = input("What is the diagonal screen size in inches? ")
 
 
-asas
-
-
-
-
-
-#size = input("What is the diagonal screen size in inches?")
+pixelDensity(size)
