52dace57a59f91b05e832a4c2346ad5effe35961
matteggers <94013259+matteggers@users.noreply.github.com>
2022-12-17 21:58:40 -0500

    Works with predetermined w/h, working to implement dimesnions in dict and resolution

--- pixelDensity.py
+++ pixelDensity.py
@@ -2,24 +2,50 @@ import math
 #coding based on formulae found in: https://www.calculatorsoup.com/calculators/technology/ppi-calculator.php
 
 
-commonDimensions = {'24': [20.918, 11.766],
-                    '24.5': [21.354, 12.011]
+#commonDimensions = {'24': [20.918, 11.766],
+#                    '24.5': [21.354, 12.011]
+#    
+#    }
+#
+#
+#
+##math.sqrt((w*w)+(h*h))
+#
+#def pixelDensity(size):
+#    values = list(commonDimensions.get(size))
+#    w = values[0]
+#    h = values[1]
+#    
+#    pixDens = math.sqrt((w*w)+(h*h))
+#    print(pixDens)
+#    return pixDens
+#    
+#
+#size = input("What is the diagonal screen size in inches? ")
+#
+#
+#pixelDensity(size)
+
+
+def diagonalPixels(size):
+    w = 1920
+    h = 1080
+    res = math.sqrt((w*w) + (h*h))
+    rounded_res = round(res, 2)
     
-    }
-
-#math.sqrt((w*w)+(h*h))
-
-def pixelDensity(size):
-    values = list(commonDimensions.get(size))
-    w = values[0]
-    h = values[1]
+    PPI = rounded_res // size
+    print(PPI)
     
-    pixDens = math.sqrt((w*w)+(h*h))
-    print(pixDens)
-    return pixDens
-    
+    #print(res)
+    return res, PPI
 
-size = input("What is the diagonal screen size in inches? ")
+size = input("Diagonal size of monitor: ")
 
 
-pixelDensity(size)
+
+diagonalPixels(int(size))
+
+
+
+#def callAll():
+#    diagonalPixels(int(size))
