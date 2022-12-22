aa1f53133c2ef566664b53eacc4cf65bb888726b
matteggers <94013259+matteggers@users.noreply.github.com>
2022-12-21 19:42:41 -0500

    Expanding project. Creating object recognition FOV of TV at distance the user sets.

---
+++ FOV/fovCalculator.py
@@ -0,0 +1,33 @@
+import math
+import numpy as np
+
+tvDimensions = {
+   
+   
+   "70": [61, 34.3],
+   "75": [65.4, 36.8]
+   
+   
+}
+
+
+user_size = int(input("What is the size of your screen?"))
+user_distance = int(input("How far will you be from the screen?"))
+user_media = int(input("Please input height of the media station the TV will sit on.")) #add option for wall mounting and exception to the 3 inch addition of stand
+view_height = 0 #make user optino to default to 42.
+
+
+def sizeCalculator():
+    if 
+    
+
+
+
+
+
+
+#create formula for height and width additions of screens based on a pillar size.
+# add option for link to bestbuy link and get exact width
+
+
+#increments of 5 increase x by [] and y by []
